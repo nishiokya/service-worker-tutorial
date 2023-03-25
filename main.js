06fecf50c4709cff19148467cc2e978da3e89474
nishiokya <nishiokya@gmail.com>
2023-03-25 15:09:07 +0900

    修正

--- main.js
+++ main.js
@@ -124,11 +124,11 @@ document.addEventListener('DOMContentLoaded', () => {
 }
 });
 if ('serviceWorker' in navigator) {
-    navigator.serviceWorker.register('/service-worker.js')
-        .then((registration) => {
-            console.log('Service Worker registered with scope:', registration.scope);
-        })
-        .catch((error) => {
-            console.error('Service Worker registration failed:', error);
-        });
+  navigator.serviceWorker.register('./service-worker.js', { scope: './' })
+      .then((registration) => {
+          console.log('Service Worker registered with scope:', registration.scope);
+      })
+      .catch((error) => {
+          console.error('Service Worker registration failed:', error);
+      });
 }
