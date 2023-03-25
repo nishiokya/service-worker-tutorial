e708a990ea648d9479e894376c423461b3b324af
nishiokya <nishiokya@gmail.com>
2023-03-25 15:05:17 +0900

    service workerの追加

--- main.js
+++ main.js
@@ -123,4 +123,12 @@ document.addEventListener('DOMContentLoaded', () => {
     });
 }
 });
-
+if ('serviceWorker' in navigator) {
+    navigator.serviceWorker.register('/serviceworker.js')
+        .then((registration) => {
+            console.log('Service Worker registered with scope:', registration.scope);
+        })
+        .catch((error) => {
+            console.error('Service Worker registration failed:', error);
+        });
+}
